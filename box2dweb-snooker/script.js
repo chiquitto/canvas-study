74b8503f6deec14c3b0fae81ee28bebd2c704798
Chiquitto <chiquitto@gmail.com>
2013-02-08 14:02:31 -0200

    Melhorando o prototipo

--- box2dweb-snooker/script.js
+++ box2dweb-snooker/script.js
@@ -5,8 +5,14 @@ var Math2 = {
 	
 	calcDensity: function(mass, volume) {
 		return mass / volume;
+	},
+	
+	getAngleDegrees: function (x1, y1, x2, y2) {
+		return Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
 	}
 }
+
+var whiteballBody;
 
 
 /**
@@ -28,20 +34,23 @@ b2DebugDraw = Box2D.Dynamics.b2DebugDraw,
 b2Fixture = Box2D.Dynamics.b2Fixture,
 b2AABB = Box2D.Collision.b2AABB;
 
-Box2D.Common.b2Settings.b2_velocityThreshold=0;
+Box2D.Common.b2Settings.b2_velocityThreshold = 0;
 
 var snooker = {
     SCALE: null,
     FPS: null,
-    RADION: null,
+    RADIUS: null,
     WIDTH: null,
     HEIGHT: null,
     CX: null,
     CY: null,
-
+	
     o: null,
     world: null,
     debugDraw: null,
+	oldPositions: {},
+	minVelocity: 0.1,
+	
     init: function() {
         this.setupConfig();
         this.setupWorld();
@@ -63,12 +72,13 @@ var snooker = {
         ballFixDef.friction = 5;
         ballFixDef.restitution = 0.1;
         ballFixDef.shape = new b2CircleShape(ballRadius * this.SCALE2 * 3);
-		
-		console.log(ballFixDef.density);
 
         // definicao da parede
         var ballBodyDef = new b2BodyDef;
         ballBodyDef.type = b2Body.b2_dynamicBody;
+		ballBodyDef.userData = 'BALL';
+		ballBodyDef.linearDamping = .6;
+        ballBodyDef.angularDamping = .6;
 		
         ballBodyDef.position.Set(this.WIDTH-1.1, this.CY);
         ballBody1 = this.world.CreateBody(ballBodyDef);
@@ -86,7 +96,7 @@ var snooker = {
     setupConfig: function() {
         this.SCALE = 100;
         this.SCALE2 = 2;
-        this.FPS = 60;
+        this.FPS = 30;
         this.RADIUS = Math.PI / 180;
         this.WIDTH = 3.1 * this.SCALE2;
         this.HEIGHT = 1.7 * this.SCALE2;
@@ -155,20 +165,22 @@ var snooker = {
         whiteballFixDef.friction = 5;
         whiteballFixDef.restitution = 0.1;
         whiteballFixDef.shape = new b2CircleShape(ballRadius * this.SCALE2 * 3);
-		
-		console.log(whiteballFixDef.density);
         
         // definicao da parede
         var whiteballBodyDef = new b2BodyDef;
         whiteballBodyDef.type = b2Body.b2_dynamicBody;
+		whiteballBodyDef.linearDamping = .7;
+        whiteballBodyDef.angularDamping = .7;
+		whiteballBodyDef.userData = 'WHITE_BALL';
         whiteballBodyDef.position.Set(1, this.CY-0.05);
         
         // cima
         whiteballBody = this.world.CreateBody(whiteballBodyDef);
         whiteballBody.CreateFixture(whiteballFixDef);
+		this.oldPositions[whiteballBodyDef.userData] = whiteballBody.GetPosition();
 
         //whiteballBody.ApplyImpulse(new b2Vec2(0.03,0), whiteballBody.GetWorldCenter());
-        whiteballBody.SetLinearVelocity(new b2Vec2(10 * this.SCALE2, 8));
+        whiteballBody.SetLinearVelocity(new b2Vec2(15 * this.SCALE2, 0 * this.SCALE2));
     },
     
     setupWorld: function() {
@@ -177,11 +189,117 @@ var snooker = {
     },
     
     update: function() {
-        this.world.Step(1/this.FPS, 100, 100);
+		/*counter++;
+		
+		if ((counter % 1) == 0) {
+			for (b = this.world.GetBodyList() ; b; b = b.GetNext()) {
+				var userData = b.GetUserData();
+				if ((userData == 'WHITE_BALL') || (userData == 'BALL')) {
+					var v = b.GetLinearVelocity();
+					var vx = v.x;
+					var vy = v.y;
+					
+					if ( (vx == 0) && (vy == 0) ) {
+						continue;
+					}
+					
+					if ((counter % 10) == 0) {
+						console.log('vx = ', vx);
+						console.log('vy = ', vy);
+					}
+					
+					vx = Math.abs(vx);
+					vy = Math.abs(vy);
+					if ( (vx < snooker.minVelocity) && (vy < snooker.minVelocity) ) {
+						b.SetLinearVelocity(new b2Vec2(0,0));
+						b.SetAngularVelocity(0);
+						continue;
+					}
+					
+					var p2 = b.GetPosition();
+					var p1 = snooker.oldPositions[userData];
+					
+					var angle = Math2.getAngleDegrees(p1.x, p1.y, p2.x, p2.y);
+					//var inverseAngle = (angle > 0) ? angle - 180 : angle + 180;
+					
+					var rel, forcaX, forcaY;
+					rel = Math.abs(angle) / 90;
+					if (angle < 0) {
+						// angle < -90
+						if (rel > 1) {
+							forcaX = rel - 1;
+							forcaY = 1 - forcaX;
+						}
+						// angle >= -90
+						else {
+							forcaY = rel;
+							forcaX = 1 - forcaY;
+						}
+					}
+					// angle > 0
+					else {
+						// angle > 90
+						if (rel > 1) {
+							forcaX = rel - 1;
+							forcaY = 1 - forcaX;
+						}
+						// angle <= 90
+						else {
+							forcaY = rel;
+							forcaX = 1 - forcaY;
+						}
+					}
+					
+					if ( (angle > 90) || (angle < -90) ) {
+						forcaX *= -1;
+					}
+					if (angle < 0) {
+						forcaY *= -1;
+					}
+					
+					var applyForce = true;
+					//if (vx < snooker.minVelocity) {
+					//	forcaX = 0;
+					//	b.SetLinearVelocity(new b2Vec2(0, vy));
+					//	applyForce = false;
+					//	console.log('b.SetLinearVelocity(new b2Vec2(0,vy));');
+					//}
+					//
+					//if (vy < snooker.minVelocity) {
+					//	forcaY = 0;
+					//	b.SetLinearVelocity(new b2Vec2(vx, 0));
+					//	applyForce = false;
+					//	console.log('b.SetLinearVelocity(new b2Vec2(vx, 0));');
+					//}
+					
+					if (applyForce) {
+						forcaX = -forcaX * 100;
+						forcaY = -forcaY * 100;
+					}
+					
+					b.ApplyForce(new b2Vec2(forcaX, forcaY), b.GetWorldCenter());
+					
+					snooker.oldPositions[userData] = {x:p2.x, y:p2.y};
+				}
+			}
+		}*/
+		
+        this.world.Step(1 / this.FPS, 4, 8);
+		
+		/*for (b = this.world.GetBodyList() ; b; b = b.GetNext()) {
+			if ((b.GetUserData() == 'WHITE_BALL') || (b.GetUserData() == 'BALL')) {
+				b.SetLinearVelocity(new b2Vec2(b.GetLinearVelocity().x * parar, b.GetLinearVelocity().y * parar));
+				b.SetAngularVelocity(b.GetAngularVelocity() * parar);
+			}
+		}*/
+		
         this.world.DrawDebugData();
         this.world.ClearForces();
     }
 }
+
+var counter = 0;
+var parar = .995;
 
 $(document).ready(function(){
     snooker.init();
